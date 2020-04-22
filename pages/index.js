4736b22337243c8b04dc46e8edf9d1b30785df6e
freeman-lab <the.freeman.lab@gmail.com>
2020-04-22 09:45:32 -0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Layout from '../components/layout'
+
+function Index (props) {
+  return (
+    <Layout>
+      Hi hi
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+
+}
+
+export default Index
