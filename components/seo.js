50d2c83d4ad4f6294e7875b8a2194e69efb728fe
freeman-lab <the.freeman.lab@gmail.com>
2020-05-19 10:25:00 -0700

    testing new social card

--- components/seo.js
+++ components/seo.js
@@ -3,18 +3,18 @@ import Head from 'next/head'
 const Seo = () => (
   <Head>
     <title>carbonplan / research</title>
-    <meta name='description' content='Data and science for carbon removal.'/>
+    <meta name='description' content='Datasets, models, interactive articles, and commentary on carbon removal and climate solutions.'/>
     <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
     <link rel='canonical' content='https://carbonplan.org/'/>
     <link rel='icon' href='https://carbonplan-assets.s3.amazonaws.com/images/favicon.svg'/>
     <link rel='apple-touch-icon' href='https://carbonplan-assets.s3.amazonaws.com/images/favicon.png'/>
     <meta property='og:title' content='carbonplan / research'/>
     <meta property="og:description" content="Data and science for carbon removal."/>
-    <meta property='og:image' content='https://carbonplan-assets.s3.amazonaws.com/images/social.png'/>
+    <meta property='og:image' content='https://carbonplan-assets.s3.amazonaws.com/images/social/research.png'/>
     <meta property="og:url" content="https://carbonplan.org"/>
     <meta name="twitter:title" content="carbonplan / research"/>
-    <meta name="twitter:description" content="Data and science for carbon removal."/>
-    <meta name="twitter:image" content="https://carbonplan-assets.s3.amazonaws.com/images/social.png"/>
+    <meta name="twitter:description" content="Datasets, models, interactive articles, and commentary on carbon removal and climate solutions."/>
+    <meta name="twitter:image" content="https://carbonplan-assets.s3.amazonaws.com/images/social/research.png"/>
     <meta name="twitter:card" content="summary_large_image"/>
   </Head>
 )
