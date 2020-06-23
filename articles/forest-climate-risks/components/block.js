17dd4caadba393a1a3428123b1987269b668b946
freeman-lab <the.freeman.lab@gmail.com>
2020-06-23 12:38:52 -0700

    add block quote formatting

---
+++ articles/forest-climate-risks/components/block.js
@@ -0,0 +1,18 @@
+import { Box, Link } from 'theme-ui'
+
+const Block = ({ children }) => {
+  return <Box sx={{
+    pl: [4],
+    borderStyle: 'solid',
+    borderColor: 'muted',
+    borderWidth: '0px',
+    borderLeftWidth: '6px',
+    fontSize: [3],
+    mt: [4],
+    mb: [4]
+  }}>
+    {children}
+  </Box>
+}
+
+export default Block
