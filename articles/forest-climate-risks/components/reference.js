7143aff9272174d3a9937c2c69be5a778710adfb
freeman-lab <the.freeman.lab@gmail.com>
2020-06-23 13:04:48 -0700

    run linter

--- articles/forest-climate-risks/components/reference.js
+++ articles/forest-climate-risks/components/reference.js
@@ -1,60 +1,71 @@
 import { Box, Link } from 'theme-ui'
 
 const Reference = () => {
-  return <Box sx={{ mt: [5], mb: [5] }}>
-    <Box sx={{
-      borderStyle: 'solid',
-      borderWidth: '0px',
-      borderTopWidth: '1px',
-      borderColor: 'muted',
-      pt: [3],
-      pb: ['22px', 3, 3],
-      textTransform: 'uppercase',
-      letterSpacing: 'wide',
-      fontFamily: 'heading',
-      fontSize: [2],
-      color: 'green'
-    }}>
-    <Box sx={{ maxWidth: '400px' }}>
-    Climate-driven risks to the climate mitigation potential of forests
+  return (
+    <Box sx={{ mt: [5], mb: [5] }}>
+      <Box
+        sx={{
+          borderStyle: 'solid',
+          borderWidth: '0px',
+          borderTopWidth: '1px',
+          borderColor: 'muted',
+          pt: [3],
+          pb: ['22px', 3, 3],
+          textTransform: 'uppercase',
+          letterSpacing: 'wide',
+          fontFamily: 'heading',
+          fontSize: [2],
+          color: 'green',
+        }}
+      >
+        <Box sx={{ maxWidth: '400px' }}>
+          Climate-driven risks to the climate mitigation potential of forests
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          borderStyle: 'solid',
+          borderWidth: '0px',
+          borderTopWidth: '1px',
+          borderColor: 'muted',
+          pt: [3],
+          pb: ['22px', 3, 3],
+          fontSize: [2],
+          fontFamily: 'faux',
+          letterSpacing: 'faux',
+        }}
+      >
+        William R. L. Anderegg, Anna T. Trugman, Grayson Badgley, Christa M.
+        Anderson, Ann Bartuska, Philippe Ciais, Danny Cullenward, Christopher B.
+        Field, Jeremy Freeman, Scott J. Goetz, Jeffrey A. Hicke, Deborah
+        Huntzinger, Robert B. Jackson, John Nickerson, Stephen Pacala, James T.
+        Randerson
+      </Box>
+      <Box
+        sx={{
+          borderStyle: 'solid',
+          borderWidth: '0px',
+          borderTopWidth: '1px',
+          borderBottomWidth: '1px',
+          borderColor: 'muted',
+          pt: [3],
+          pb: ['22px', 3, 3],
+          fontSize: [2],
+          fontFamily: 'faux',
+          letterSpacing: 'faux',
+          color: 'secondary',
+        }}
+      >
+        Science, 368 (2020){' '}
+        <Link
+          sx={{ color: 'secondary' }}
+          href='https://dx.doi.org/10.1126/science.aaz7005'
+        >
+          10.1126/science.aaz7005
+        </Link>
+      </Box>
     </Box>
-    </Box>
-    <Box sx={{
-      borderStyle: 'solid',
-      borderWidth: '0px',
-      borderTopWidth: '1px',
-      borderColor: 'muted',
-      pt: [3],
-      pb: ['22px', 3, 3],
-      fontSize: [2],
-      fontFamily: 'faux',
-      letterSpacing: 'faux'
-    }}>
-    William R. L. Anderegg, Anna T. Trugman, Grayson Badgley, Christa M. Anderson,
-    Ann Bartuska, Philippe Ciais, Danny Cullenward, Christopher B. Field, Jeremy Freeman, 
-    Scott J. Goetz, Jeffrey A. Hicke, Deborah Huntzinger, Robert B. Jackson, 
-    John Nickerson, Stephen Pacala, James T. Randerson
-    </Box>
-    <Box sx={{
-      borderStyle: 'solid',
-      borderWidth: '0px',
-      borderTopWidth: '1px',
-      borderBottomWidth: '1px',
-      borderColor: 'muted',
-      pt: [3],
-      pb: ['22px', 3, 3],
-      fontSize: [2],
-      fontFamily: 'faux',
-      letterSpacing: 'faux',
-      color: 'secondary'
-    }}>
-    Science, 368 (2020) {' '}
-      <Link sx={{ color: 'secondary' }}href='https://dx.doi.org/10.1126/science.aaz7005'>
-        10.1126/science.aaz7005
-      </Link>
-    </Box>
-    
-  </Box>
+  )
 }
 
 export default Reference
--- articles/forest-climate-risks/index.md
+++ articles/forest-climate-risks/index.md
@@ -11,14 +11,22 @@ export const meta = {
 
 # Climate-driven risks to forest carbon
 
-Team members Jeremy Freeman and Danny Cullenward contributed to a new [review article on forest carbon risks](https://doi.org/10.1126/science.aaz7005) that was published in *Science* magazine and led by our collaborators Bill Anderegg (University of Utah), Anna Trugman (UC Santa Barbara), and Grayson Badgley (Black Rock Forest Consortium / Columbia University).
+Team members Jeremy Freeman and Danny Cullenward contributed to a new [review article on forest carbon risks](https://doi.org/10.1126/science.aaz7005) that was published in _Science_ magazine and led by our collaborators Bill Anderegg (University of Utah), Anna Trugman (UC Santa Barbara), and Grayson Badgley (Black Rock Forest Consortium / Columbia University).
 
-<Reference/>
+<Reference />
 
-Here’s how the editors at *Science* summarized the article:
+Here’s how the editors at _Science_ summarized the article:
 
 <Block>
-Much recent attention has focused on the potential of trees and forests to mitigate ongoing climate change by acting as sinks for carbon. Anderegg et al. review the growing evidence that forests' climate mitigation potential is increasingly at risk from a range of adversities that limit forest growth and health. These include physical factors such as drought and fire and biotic factors, including the depredations of insect herbivores and fungal pathogens. Full assessment and quantification of these risks, which themselves are influenced by climate, is key to achieving science-based policy outcomes for effective land and forest management.
+  Much recent attention has focused on the potential of trees and forests to
+  mitigate ongoing climate change by acting as sinks for carbon. Anderegg et al.
+  review the growing evidence that forests' climate mitigation potential is
+  increasingly at risk from a range of adversities that limit forest growth and
+  health. These include physical factors such as drought and fire and biotic
+  factors, including the depredations of insect herbivores and fungal pathogens.
+  Full assessment and quantification of these risks, which themselves are
+  influenced by climate, is key to achieving science-based policy outcomes for
+  effective land and forest management.
 </Block>
 
 We’re excited to share this publication. We're also excited to start work on new open source tools that apply the paper’s insights to evaluate both the benefits of and risks to forest carbon strategies. Getting the details right — particularly as the risks evolve in a changing climate — is essential for making smart climate decisions today.
