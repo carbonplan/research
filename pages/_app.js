ff8036d2a915795181dab067bee79c54643b04d9
freeman-lab <the.freeman.lab@gmail.com>
2020-07-25 20:16:24 -0700

    make target blank for default links

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import App from 'next/app'
+import { Link } from 'theme-ui'
 import { Style } from '@carbonplan/components'
 import { ThemeProvider } from 'theme-ui'
 import { MDXProvider } from '@mdx-js/react'
 
 import theme from '../theme'
 
+const components = {
+  a: (props) => <Link target='_blank' {...props} />
+}
+
 export default ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
-      <MDXProvider>
+      <MDXProvider components={components}>
         <Component {...pageProps} />
         <Style />
       </MDXProvider>
