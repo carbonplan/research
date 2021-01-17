2e8e3b9bebbdfdec394e03f2ab10f47e244d2296
freeman-lab <the.freeman.lab@gmail.com>
2021-01-17 14:31:36 -0800

    tiny spacing fix

--- components/reference.js
+++ components/reference.js
@@ -65,6 +65,7 @@ const Reference = ({ color, data }) => {
               fontFamily: 'body',
               fontSize: [1],
               lineHeight: 1.25,
+              letterSpacing: '0.0125em',
               color: color,
               opacity: [1, 1, selected ? 1 : 0.5],
               display: 'inline-block',
@@ -76,12 +77,20 @@ const Reference = ({ color, data }) => {
               sx={{
                 ml: ['-18px'],
                 lineHeight: 1.25,
+                letterSpacing: '0.0125em',
                 display: ['none', 'none', 'initial'],
               }}
             >
               {data.number}
             </Text>
-            <Text as='span' sx={{ ml: [0, 0, '7px'], lineHeight: 1.25 }}>
+            <Text
+              as='span'
+              sx={{
+                ml: [0, 0, '7px'],
+                lineHeight: 1.25,
+                letterSpacing: '0.0125em',
+              }}
+            >
               {' '}
               {data.authors} {data.year ? `(${data.year})` : ''} {data.title}{' '}
               <i>{data.journal}</i>
