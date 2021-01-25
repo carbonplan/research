056f17836b2a2e9ad5cf6347346ef2300cbc9615
freeman-lab <the.freeman.lab@gmail.com>
2021-01-24 16:33:33 -0800

    add lang

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ import { InitializeColorMode } from 'theme-ui'
 class MyDocument extends Document {
   render() {
     return (
-      <Html className='no-focus-outline'>
+      <Html lang='en' className='no-focus-outline'>
         <Head>
           <Tracking />
         </Head>
