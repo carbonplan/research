3dd87f77b3372f167867a9e8fb768d0067f26752
freeman-lab <the.freeman.lab@gmail.com>
2021-01-28 22:30:17 -0800

    fix mobile bar width

--- tools/dac-calculator/components/charts/param-chart.js
+++ tools/dac-calculator/components/charts/param-chart.js
@@ -15,7 +15,7 @@ const ParamChart = ({ param, data }) => {
   useEffect(() => {
     if (document.body.clientWidth < 420) {
       setWidth(280)
-      setBarWidth(8)
+      setBarWidth(11)
     }
   }, [])
 
