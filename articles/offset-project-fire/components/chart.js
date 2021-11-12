f41407ab2a23dd19a95f7936b3499271be49ae0d
Kata Martin <katasm@gmail.com>
2021-11-12 12:03:25 -0800

    Visual tweaks

--- articles/offset-project-fire/components/chart.js
+++ articles/offset-project-fire/components/chart.js
@@ -15,27 +15,34 @@ const Chart = ({ data }) => {
     <Box
       sx={{
         width: '100%',
-        height: ['250px', '275px', '300px', '400px'],
+        height: ['250px', '275px', '275px', '400px'],
         my: [4, 4, 4, 5],
       }}
     >
       <ChartComponent x={[2018, 2103]} y={[0, 1.1]}>
         <Ticks bottom values={YEAR_TICKS} />
-        <TickLabels bottom values={YEAR_TICKS} />
-        <AxisLabel align='center' bottom>
-          Year
-        </AxisLabel>
-
+        <Box sx={{ display: ['inherit', 'none', 'none', 'none'] }}>
+          <TickLabels
+            bottom
+            values={YEAR_TICKS.filter((_, i) => i % 2 === 0)}
+          />
+        </Box>
+        <Box sx={{ display: ['none', 'inherit', 'inherit', 'inherit'] }}>
+          <TickLabels bottom values={YEAR_TICKS} />
+        </Box>
+        <AxisLabel bottom>Year</AxisLabel>
         <Ticks left />
         <TickLabels left />
-        <AxisLabel align='center' left>
+        <AxisLabel align='left' left>
           Fraction buffer pool
         </AxisLabel>
 
         <Axis bottom left />
         <Plot>
           {data &&
-            data.map((line, i) => <Line key={i} data={line} color='red' />)}
+            data.map((line, i) => (
+              <Line width={1.5} key={i} data={line} color='red' />
+            ))}
           <Line
             color='gray'
             sx={{ 'stroke-dasharray': '8 4' }}
