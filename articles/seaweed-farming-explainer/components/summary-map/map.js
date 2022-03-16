317cf35ec2f1dace0458cc86c585826e81da3b49
Kata Martin <katasm@gmail.com>
2022-03-16 11:59:25 -0400

    Override projection glsl to avoid strips of map

--- articles/seaweed-farming-explainer/components/summary-map/map.js
+++ articles/seaweed-farming-explainer/components/summary-map/map.js
@@ -12,6 +12,42 @@ const VARIABLES = {
   sinking: 'netcostpertonsink',
 }
 
+const getCustomProjection = () => {
+  return {
+    ...naturalEarth1(),
+    glsl: {
+      func: `
+      vec2 naturalEarth1Invert(float x, float y)
+      {
+        const float pi = 3.14159265358979323846264;
+        const float halfPi = pi * 0.5;
+        float phi = y;
+        float delta;
+        float phi2 = phi * phi;
+        float phi4 = phi2 * phi2;
+        for (int i = 0; i < 25; i++) {
+          phi2 = phi * phi;
+          phi4 = phi2 * phi2;
+          delta = (phi * (1.007226 + phi2 * (0.015085 + phi4 * (-0.044475 + 0.028874 * phi2 - 0.005916 * phi4))) - y) / (1.007226 + phi2 * (0.015085 * 3.0 + phi4 * (-0.044475 * 7.0 + 0.028874 * 9.0 * phi2 - 0.005916 * 11.0 * phi4)));
+          phi = phi - delta;
+          if (abs(delta) < 1e-6) {
+            break;
+          }
+        }
+        phi2 = phi * phi;
+        float lambda = x / (0.8707 + phi2 * (-0.131979 + phi2 * (-0.013791 + phi2 * phi2 * phi2 * (0.003971 - 0.001529 * phi2))));
+        if (lambda <= -1.0 * pi + 1e-6 || lambda >= pi - 1e-6) {
+          return vec2(-1000.0, -1000.0);
+        } else {
+          return vec2(degrees(lambda), degrees(phi));
+        }
+      }
+    `,
+      name: 'naturalEarth1Invert',
+    },
+  }
+}
+
 const Map = () => {
   const { theme } = useThemeUI()
   const [map, setMap] = useState('sinking')
@@ -46,7 +82,7 @@ const Map = () => {
         />
       </Flex>
       <Box sx={{ display: 'block', width: '100%', position: 'relative' }}>
-        <Minimap projection={naturalEarth1}>
+        <Minimap projection={getCustomProjection}>
           <Path
             stroke={theme.colors.primary}
             source={'https://cdn.jsdelivr.net/npm/world-atlas@2/land-110m.json'}
