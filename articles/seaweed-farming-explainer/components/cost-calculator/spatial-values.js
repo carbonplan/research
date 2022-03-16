90b6235ccb0dfbb4cbfdc7651f025c977da93f06
Kata Martin <katasm@gmail.com>
2022-03-16 12:23:27 -0400

    Fix calculator map positioning + sizing

--- articles/seaweed-farming-explainer/components/cost-calculator/spatial-values.js
+++ articles/seaweed-farming-explainer/components/cost-calculator/spatial-values.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react'
-import { Box, Flex } from 'theme-ui'
-import { Badge } from '@carbonplan/components'
+import { Box } from 'theme-ui'
+import { Badge, Column, Row } from '@carbonplan/components'
 import { formatValue } from './utils'
 import Map from './map'
 
@@ -15,7 +15,7 @@ const MapDescription = ({ color, target, values }) => {
 
   return (
     <Box sx={{ fontFamily: 'faux', letterSpacing: 'faux' }}>
-      At this location{' '}
+      At this location,{' '}
       <Badge sx={badgeSx}>{formatValue(values.seaweed_dw)}</Badge> tons DW / km²
       of <Badge sx={badgeSx}>{values.species}</Badge> seaweed could be grown,
       requiring <Badge sx={badgeSx}>{values.nharv}</Badge> harvest
@@ -69,24 +69,26 @@ const SpatialValues = ({ location, locations, setLocation, sx, target }) => {
   return (
     <Box>
       <Box sx={sx.heading}>Spatial values</Box>
-
-      <Flex
-        sx={{
-          flexDirection: ['column', 'row-reverse', 'column', 'column'],
-          gap: ['12px', 0, 4, 4],
-        }}
-      >
-        <Map
-          location={location}
-          locations={locations}
-          setLocation={handleLocationChange}
-        />
-        <MapDescription
-          values={locations[location].values}
-          target={target}
-          color={color}
-        />
-      </Flex>
+      <Row columns={[6, 6, 3, 3]}>
+        <Column
+          width={[6, 3, 3, 3]}
+          start={[1, 4, 1, 1]}
+          sx={{ order: [1, 2, 1, 1], mb: ['12px', 0, 4, 4] }}
+        >
+          <Map
+            location={location}
+            locations={locations}
+            setLocation={handleLocationChange}
+          />
+        </Column>
+        <Column width={[6, 3, 3, 3]} start={1} sx={{ order: [2, 1, 2, 2] }}>
+          <MapDescription
+            values={locations[location].values}
+            target={target}
+            color={color}
+          />
+        </Column>
+      </Row>
     </Box>
   )
 }
