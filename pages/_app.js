03c687c91abf9a1401f11ad8582f6a8bdaafeb63
Kata Martin <katasm@gmail.com>
2022-05-02 12:12:55 -0700

    Set up custom scroll context

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,18 @@
 import { ThemeProvider } from 'theme-ui'
 import { MDXProvider } from '@mdx-js/react'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import '@carbonplan/components/fonts.css'
 import '@carbonplan/components/globals.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import theme from '@carbonplan/theme'
+import { ScrollProvider } from '../components/scroll'
 
 const App = ({ Component, pageProps }) => {
-  const router = useRouter()
-  useEffect(() => {
-    // Ensure that automatic scroll restoration is used on browser navigation
-    router.beforePopState(() => {
-      history.scrollRestoration = 'auto'
-      return true
-    })
-  }, [])
   return (
     <ThemeProvider theme={theme}>
       <MDXProvider>
-        <Component {...pageProps} />
+        <ScrollProvider>
+          <Component {...pageProps} />
+        </ScrollProvider>
       </MDXProvider>
     </ThemeProvider>
   )
