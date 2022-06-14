b3bd3597f86c3e9eddab23c367a13205146c83e6
Kata Martin <katasm@gmail.com>
2022-06-14 15:14:47 -0700

    Use layouts@2.3.0-develop.3

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const isDev =
   process.env.VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development'
 
 const slug = require('rehype-slug')
+const path = require('path')
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
@@ -15,5 +16,23 @@ module.exports = withMDX({
   assetPrefix: isDev ? '' : 'https://research.carbonplan.org',
   experimental: {
     scrollRestoration: true,
+  },
+  webpack: (config, options) => {
+    if (options.isServer) {
+      config.externals = ['react', 'theme-ui', ...config.externals]
+    }
+    config.resolve.alias['react'] = path.resolve(
+      __dirname,
+      '.',
+      'node_modules',
+      'react'
+    )
+    config.resolve.alias['theme-ui'] = path.resolve(
+      __dirname,
+      '.',
+      'node_modules',
+      'theme-ui'
+    )
+    return config
   },
 })
