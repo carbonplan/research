2a04f510cee97bf97847ea552e3ab699781ea09e
Kata Martin <katasm@gmail.com>
2022-06-16 15:19:46 -0700

    Remove accidental next.config change

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const isDev =
   process.env.VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development'
 
 const slug = require('rehype-slug')
-const path = require('path')
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
@@ -16,23 +15,5 @@ module.exports = withMDX({
   assetPrefix: isDev ? '' : 'https://research.carbonplan.org',
   experimental: {
     scrollRestoration: true,
-  },
-  webpack: (config, options) => {
-    if (options.isServer) {
-      config.externals = ['react', 'theme-ui', ...config.externals]
-    }
-    config.resolve.alias['react'] = path.resolve(
-      __dirname,
-      '.',
-      'node_modules',
-      'react'
-    )
-    config.resolve.alias['theme-ui'] = path.resolve(
-      __dirname,
-      '.',
-      'node_modules',
-      'theme-ui'
-    )
-    return config
   },
 })
