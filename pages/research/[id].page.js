340b3508b3e4f13a6cd2e4818ca8fb30720194b9
katamartin <katasm@gmail.com>
2022-09-16 11:29:16 -0700

    Handle hashed elements

--- pages/research/[id].page.js
+++ pages/research/[id].page.js
@@ -3,7 +3,8 @@ import matter from 'gray-matter'
 import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
 import { useMDXComponents } from '@mdx-js/react'
-
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { Box } from 'theme-ui'
 import {
   Article,
@@ -54,6 +55,13 @@ const SUPPLEMENT_COMPONENTS = {
 
 const Page = ({ id, type, source, frontMatter, references }) => {
   const components = useMDXComponents()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (window.location.hash) {
+      router.push({ hash: window.location.hash })
+    }
+  }, [])
 
   switch (type) {
     case 'article':
