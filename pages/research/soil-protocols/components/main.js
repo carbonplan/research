b9469770dc098061a55159a45fa0bd66bf773ad2
katamartin <katasm@gmail.com>
2022-10-27 09:16:39 -0700

    Fix some tools

--- pages/research/soil-protocols/components/main.js
+++ pages/research/soil-protocols/components/main.js
@@ -1,4 +1,4 @@
-import { Box, Themed, Divider } from 'theme-ui'
+import { Box, Divider } from 'theme-ui'
 import { useBreakpointIndex } from '@theme-ui/match-media'
 import { Row, Column } from '@carbonplan/components'
 import Table from './table'
@@ -19,11 +19,15 @@ const Main = () => {
       <Divider sx={{ mt: [6, 7, 8, 9] }} />
       <Row columns={[6, 8, 10, 10]}>
         <Column start={[1]} width={[6, 4, 5, 5]}>
-          <Themed.h2>About</Themed.h2>
+          <Box as='h2' variant='styles.h2'>
+            About
+          </Box>
           <About />
         </Column>
         <Column start={[1, 5, 6, 6]} width={[6, 4, 5, 5]}>
-          <Themed.h2>Methods</Themed.h2>
+          <Box as='h2' variant='styles.h2'>
+            Methods
+          </Box>
           <Methods />
         </Column>
       </Row>
