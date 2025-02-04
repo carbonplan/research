9e74983f3e1aaa347d74f0234434c6e7b6581891
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-02-04 11:11:13 -0600

    temporarily keep GA

--- pages/_document.page.js
+++ pages/_document.page.js
@@ -1,11 +1,14 @@
 import Document, { Html, Main, NextScript, Head } from 'next/document'
+import { Tracking } from '@carbonplan/components'
 import { InitializeColorMode } from 'theme-ui'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang='en' className='no-focus-outline'>
-        <Head />
+        <Head>
+          <Tracking id={process.env.GA_TRACKING_ID} />
+        </Head>
         <body>
           <InitializeColorMode />
           <Main />
